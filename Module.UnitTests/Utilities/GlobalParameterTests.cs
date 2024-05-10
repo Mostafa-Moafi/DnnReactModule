ee4e2a9a233d707126f7b8dfd3758f3d2c6e661d
Mostafa Moafi <moafimostafa80@gmail.com>
2024-05-10 20:51:57 +0330

    Test for const data

---
+++ Module.UnitTests/Utilities/GlobalParameterTests.cs
@@ -0,0 +1,16 @@
+﻿using DnnReactModule.Utilities;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Module.UnitTests.Utilities
+{
+    public class GlobalParameterTests
+    {
+        [Fact]
+        public void ModuleNameIsValid()
+        {
+            var moduleName = GlobalParameter.ModuleName;
+            Assert.Equal(expected: "DnnReactModule", moduleName);
+        }
+    }
+}
